46ff5a04e8c65996920a9cc4139cd071c0d8753c
myrionsc <marpht23@gmail.com>
2019-03-07 12:51:18 +0100

    moving background!

--- Typecraft/src/app/models/scenes/gameScene.ts
+++ Typecraft/src/app/models/scenes/gameScene.ts
@@ -2,7 +2,7 @@ import {Player} from '../objects/player';
 import {Enemy} from '../objects/enemy';
 
 export class GameScene extends Phaser.Scene {
-    private background: Phaser.GameObjects.Image;
+    private background: Phaser.GameObjects.TileSprite;
     private player: Player;
     private enemy: Enemy;
 
@@ -13,22 +13,24 @@ export class GameScene extends Phaser.Scene {
     }
 
     preload(): void {
-        this.load.image('background', 'assets/background.png');
+        // this.load.image('background', 'assets/background.png');
+        this.load.image('background', 'assets/Background/grasstile.png');
         this.load.image('player', 'assets/player.png');
         this.load.image('enemy', 'assets/ZergZerglingPurple.png');
     }
 
     create(): void {
         // create background
-        this.background = this.add.image(0, 0, 'background');
-        this.background.setOrigin(0, 0);
+        this.background = this.add.tileSprite(0, 0, 1800, 9000, 'background');
 
         this.player = new Player({scene: this, x: 150, y: 300, key: 'player'});
-
         this.enemy = new Enemy({scene: this, x: 150, y: 30, key: 'enemy'});
     }
 
     update(): void {
+        // scroll background
+        this.background.tilePositionY -= 1;
+
         this.player.update();
         this.enemy.update();
     }
