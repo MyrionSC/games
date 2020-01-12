39d23ef93122ce9069cc8da40372faf5603a6ee9
myrionsc <marpht23@gmail.com>
2020-01-13 00:30:19 +0100

    deploy stuff

--- Swordcraft/deploy-prod.sh
+++ Swordcraft/deploy-prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "starting deploy"
-/usr/bin/rsync -ratlz --info=progress2 dist/. marand@marand.dk:/var/www/html/swordcraft
+/usr/bin/rsync -ratlz --info=progress2 dist/. marand@marand.dk:/var/www/html/games/swordcraft
 echo "deploy done"
 
