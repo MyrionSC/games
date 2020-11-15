62e5a7c37cff8502083cfff3d1ac1130e8796949
Martin RA <marpht23@gmail.com>
2020-11-15 15:19:50 +0100

    Swordcraft: updated deploy

--- Swordcraft/deploy-prod.sh
+++ Swordcraft/deploy-prod.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-echo "starting deploy"
-ssh marand@marand.dk "rm -r /var/www/html/games/swordcraft"
-/usr/bin/rsync -ratlz --info=progress2 dist/. marand@marand.dk:/var/www/html/games/swordcraft
-echo "deploy done"
+scp -r dist marand:/var/www/html/games/swordcraft
 
